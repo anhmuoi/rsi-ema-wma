0c7d48b1522e997fd9b01ae9e34e349026f7d8c1
anhmuoi <anhmuoi200147@gmail.com>
2022-03-19 10:49:17 +0700

    edit signal

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import schedule
 import pandas as pd
 import talib as ta
 import math as math
+from datetime import datetime  
+from datetime import timedelta  
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -17,11 +19,11 @@ import time
 SYMBOL = 'BTC/USDT'
 SYMBOL_FREE = 'BTC'
 LIMIT = 1000
-ANGLE = 20
+ANGLE = 10
 
 TIME_FRAME = '5m'
 
-SLOPE = 5
+SLOPE = 10
 #this is the slope of rsi
 
 
@@ -72,40 +74,121 @@ def rsi_signal(df):
 
     for i in range(45,len(df)-1):
         tmp = (df['rsi'][i] + df['rsi'][i+1]) / 2
-        if (((abs(df['rsi'][i] - df['rsi_wma'][i]) < 1.5) and (abs(df['rsi'][i] - df['rsi_ema'][i])) < 1.5) or ((abs(tmp - df['rsi_wma'][i]) < 1.5) and (abs(tmp - df['rsi_ema'][i]) < 1.5))) and (abs(df['rsi_ema'][i] - df['rsi_wma'][i]) < 1.5):
+        if (((abs(df['rsi'][i] - df['rsi_wma'][i]) < 3) and (abs(df['rsi'][i] - df['rsi_ema'][i])) < 3) or ((abs(tmp - df['rsi_wma'][i]) < 3.5) and (abs(tmp - df['rsi_ema'][i]) < 3))) and (abs(df['rsi_ema'][i] - df['rsi_wma'][i]) < 3.5) :
             df.at[i, 'rsi_start'] = True
             if (df['rsi'][i+1] < df['rsi_wma'][i+1]) and (printAngle((20, df['rsi'][i]),(22, df['rsi_ema'][i+1]),(22, df['rsi'][i+1])) > ANGLE):
                 count = 0
                 for j in range(i+1, len(df)-1):
                     if (df['rsi_start'][i] == True):
                         if(((df['rsi'][j] == df['rsi_ema'][j]) or ((df['rsi_ema'][j] - df['rsi'][j]>0) and (df['rsi_ema'][j+1] - df['rsi'][j+1]<0))) and (abs(df['rsi'][i] - df['rsi'][j]) > SLOPE)) and count<2:
-                            df.at[j+1, 'start_buy'] = True
-                            df.at[j+1, 'start_sell'] = False
-                            count = count + 1
-                            print(abs(df['rsi'][i] - df['rsi'][j]),i,j,'buy')
+                            if(count == 0):
+                                tmp = df['rsi'][i]
+                                tmp_index = 0
+                                for k in range(i+1, j):
+                                    
+                                    if(df['rsi'][k] < df['rsi_ema'][k]):
+                                        if (k>i+1) and (abs(df['rsi'][k] - df['rsi_wma'][k]) < 1):
+                                            tmp = df['rsi'][i]
+                                            break 
+                                        tmp_index +=1
+                                        tmp = df['rsi'][k]
+                                    else:
+                                        break
+                                if (df['rsi'][i] - tmp > 20):
+                                    df.at[j+1, 'start_buy'] = True
+                                    df.at[j+1, 'start_sell'] = False
+                                    count = count + 1
+                                    print(abs(df['rsi'][i] - df['rsi'][j]),i,j,tmp,'buy')
+                                else:
+                                    print(tmp,df['timestamp'][i])
+                            elif count == 1:
+                                df.at[j+1, 'start_buy'] = True
+                                df.at[j+1, 'start_sell'] = False
+                                count = count + 1
+                                
                         elif (df['rsi'][j] > df['rsi_wma'][j]) or count == 2:
                             df.at[i, 'rsi_start'] = False
                             count = 0
                         if(df['rsi'][len(df)-1] == df['rsi_ema'][len(df)-1]) or ((df['rsi_ema'][len(df)-2] - df['rsi'][len(df)-2]>0) and (df['rsi_ema'][len(df)-1] - df['rsi'][len(df)-1]<0)) and count<2 and j == len(df)-2:
-                            df.at[len(df)-1, 'start_buy'] = True
-                            df.at[len(df)-1, 'start_sell'] = False
-                            print(abs(df['rsi'][i] - df['rsi'][j]),i,j,'buy')
+                            if count == 0:
+                                tmp = df['rsi'][i]
+                                tmp_index = 0
+                                for k in range(i+1, len(df)-1):
+                                    
+                                    if (df['rsi'][k] < df['rsi_ema'][k]):
+                                        if (k>i+1) and (abs(df['rsi'][k] - df['rsi_wma'][k]) < 1):
+                                            tmp = df['rsi'][i]
+                                            break 
+                                        tmp_index +=1
+                                        tmp = df['rsi'][k]
+                                    else:
+                                        break
+                                if (df['rsi'][i] - tmp > 20):
+                                    df.at[len(df)-1, 'start_buy'] = True
+                                    df.at[len(df)-1, 'start_sell'] = False
+                                    print(abs(df['rsi'][i] - df['rsi'][j]),i,len(df)-1,tmp,'buy')
+                            elif count == 1:
+                                df.at[len(df)-1, 'start_buy'] = True
+                                df.at[len(df)-1, 'start_sell'] = False
+                                count = count + 1
+                                
+
             elif (df['rsi'][i+1] > df['rsi_wma'][i+1]) and (printAngle((20, df['rsi'][i]),(25, df['rsi_ema'][i+1]),(25, df['rsi'][i+1])) > ANGLE):
                 count = 0
                 for j in range(i+1, len(df)-1):
                     if (df['rsi_start'][i] == True):
                         if(((df['rsi'][j] == df['rsi_ema'][j]) or ((df['rsi_ema'][j] - df['rsi'][j]<0) and (df['rsi_ema'][j+1] - df['rsi'][j+1]>0))) and (abs(df['rsi'][i] - df['rsi'][j]) > SLOPE)) and count<2:
-                            df.at[j+1, 'start_sell'] = True
-                            df.at[j+1, 'start_buy'] = False
-                            count = count + 1
-                            print(abs(df['rsi'][i] - df['rsi'][j]),i,j,'sell')
+                            if (count == 0):
+                                tmp_s = df['rsi'][i]
+                                tmp_s_index = 0
+                                for k in range(i+1, j):
+                                    
+                                    if (df['rsi'][k] > df['rsi_ema'][k]):
+                                        if (k>i+1) and (abs(df['rsi'][k] - df['rsi_wma'][k]) < 1):
+                                            tmp_s = df['rsi'][i]
+                                            break 
+                                        tmp_s_index+=1
+                                        tmp_s = df['rsi'][k]
+                                    else:
+                                        print(tmp_s,df['timestamp'][i],'break')
+                                        break
+                                if (tmp_s - df['rsi'][i] > 18):
+                                    df.at[j+1, 'start_sell'] = True
+                                    df.at[j+1, 'start_buy'] = False
+                                    print(tmp_s_index)
+                                    count = count + 1
+                                    print(abs(df['rsi'][i] - df['rsi'][[len(df)-1]]),i,[len(df)-1],'sell')
+                            elif count == 1:
+                                df.at[j+1, 'start_sell'] = True
+                                df.at[j+1, 'start_buy'] = False 
+                                count = count + 1      
                         elif (df['rsi'][j] < df['rsi_wma'][j]) or count == 2:
                             df.at[i, 'rsi_start'] = False 
                             count = 0
                         if(df['rsi'][len(df)-1] == df['rsi_ema'][len(df)-1]) or ((df['rsi_ema'][len(df)-2] - df['rsi'][len(df)-2]<0) and (df['rsi_ema'][len(df)-1] - df['rsi'][len(df)-1]>0)) and count<2 and j == len(df)-2:
-                            df.at[len(df)-1, 'start_sell'] = True
-                            df.at[len(df)-1, 'start_buy'] = False
-                            print(abs(df['rsi'][i] - df['rsi'][j]),i,j,'sell')
+                            if (count == 0):
+                                tmp_s = df['rsi'][i]
+                                tmp_s_index = 0
+                                for k in range(i+1, len(df)-1):
+                                    
+                                    if (df['rsi'][k] > df['rsi_ema'][k]):
+                                        if (k>i+1) and (abs(df['rsi'][k] - df['rsi_wma'][k]) < 1):
+                                            tmp_s = df['rsi'][i]
+                                            break 
+                                        tmp_s_index+=1
+                                        tmp_s = df['rsi'][k]
+                                    else:
+                                        break
+                                if (tmp_s - df['rsi'][i] > 20):
+                                    df.at[[len(df)-1], 'start_sell'] = True
+                                    df.at[[len(df)-1], 'start_buy'] = False
+                                    print(tmp_s_index)
+                                    count = count + 1
+                                    print(abs(df['rsi'][i] - df['rsi'][[len(df)-1]]),i,[len(df)-1],'sell')
+                            elif count == 1:
+                                df.at[[len(df)-1], 'start_sell'] = True
+                                df.at[[len(df)-1], 'start_buy'] = False 
+                                count = count + 1      
     return df
 
 stoploss = 0
@@ -113,6 +196,8 @@ takeprofit = 0
 rsi_tmp = 0
 buy = False    
 sell = False
+index_buy = 0
+index_sell = 0
 # check buy sell signals
 def check_buy_sell_signals(df):
     last_row_index = len(df.index) - 1
@@ -177,7 +262,9 @@ def run_bot():
     bars = exchange.fetch_ohlcv(SYMBOL, timeframe=TIME_FRAME, limit=LIMIT)
 
     df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df['timestamp'] = pd.to_datetime(df['timestamp'] , unit='ms')
+    df['timestamp'] = df['timestamp'] + timedelta(hours=7)
+
 
     # rsi
     df['rsi'] = ta.RSI(df['close'], timeperiod=14)
